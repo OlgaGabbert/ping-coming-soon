ea549f8815335834ecd7baf98dc37580d93d9522
Olga Gabbert <olga.gabbert@gmail.com>
2023-01-16 21:40:29 +0100

    minor js changes

--- main.js
+++ main.js
@@ -7,22 +7,32 @@ let illustration = document.getElementById('illustration');
 
 function myFunction() {
     let myInput = document.getElementById('input').value;
-    console.log(myInput);
-    if (myInput.match(validRegex)) {
+    if(myInput == "") {
+        if(!document.getElementById('newDiv')) {
+            if (!document.getElementById('newDivError')) {
+                let newDiv = document.createElement("div");
+                newDiv.id = 'newDiv';
+                newDiv.innerHTML = "Whoops! It looks like you forgot to add your email";
+                form.append(newDiv);
+                newDiv.classList.add('new');
+                button.classList.add('newButton');
+                buttonText.classList.add('newButtonText');
+                inputForm.classList.add('newInput');
+                illustration.classList.add('newIllustration');
+            }
+        }
+    } else if (myInput.match(validRegex)) {
         form.innerHTML = 'Thank you!';
-    } else { 
-        if (!document.getElementById("newDiv")) {
-            let newDiv = document.createElement("div");
-        newDiv.id = 'newDiv';
-        newDiv.innerHTML = "Please provide a valid email address";
-        form.append(newDiv);
-        newDiv.classList.add('new');
+    } else if (!document.getElementById("newDivError")) {
+        let newDivError = document.createElement("div");
+        newDivError.id = 'newDivError';
+        newDivError.innerHTML = "Please provide a valid email address";
+        form.append(newDivError);
+        newDivError.classList.add('new');
         button.classList.add('newButton');
         buttonText.classList.add('newButtonText');
         inputForm.classList.add('newInput');
         illustration.classList.add('newIllustration');
-        } else {
-
-        }
     }
-  }
+}
+ 
